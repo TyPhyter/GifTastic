cebd51a4512ebf3b9c270c8256b759a65c05a9c5
TyPhyter <tylerjenkins08@gmail.com>
2018-03-11 14:14:34 -0500

    buttons dynamically generating from array, document click listener working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
- 
+var topics = [
+    "adventure time",
+    "spongebob squarepants",
+    "rick and morty",
+];
+//generate div to hold butotns
+var buttonDiv = $("<div></div>");
+//for every topic, create a button with the index data attribute and the topic text, append to buttonDiv
+for (var i = 0; i < topics.length; i++){
+    var newButton = $(`<button class="gif-button" data-index=${i}>${topics[i]}</button>`);
+    buttonDiv.append(newButton);
+}
+//append buttonDiv to target div
+$("#buttons-target").append(buttonDiv);
+
+$(document).on("click", ".gif-button", function(){
+    alert("button clicked");
+});

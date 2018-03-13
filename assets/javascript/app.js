f0825d124bbdcca1c1a59606b6ed291eae906334
TyPhyter <tylerjenkins08@gmail.com>
2018-03-13 18:49:54 -0500

    add button finished, styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,21 @@
 var $buttonTarget = $("#button-target");
 var $gifTarget = $("#gif-target");
+var $addButton = $("#add-button");
+var $addInput = $("#add-input");
 
 var topics = [
-    "adventure time",
-    "spongebob squarepants",
-    "rick and morty",
+    "Adventure Time",
+    "Spongebob Squarepants",
+    "Rick and Morty",
+    "Regular Show",
+    "Bravest Warriors"
 ];
+
 //generate div to hold butotns
 var buttonDiv = $("<div></div>");
 //for every topic, create a button with the index data attribute and the topic text, append to buttonDiv
 for (var i = 0; i < topics.length; i++){
-    var newButton = $(`<button class="gif-button" data-index=${i}>${topics[i]}</button>`);
+    var newButton = $(`<button class="gif-button btn btn-primary" data-index=${i}>${topics[i]}</button>`);
     buttonDiv.append(newButton);
 }
 //append buttonDiv to target div
@@ -22,7 +27,7 @@ $(document).on("click", ".gif-button", function(){
     var params = $.param({
         api_key: "fuu2Wu6FmADeffdF7V1cHgBhCBw2VBQA",
         q : $(this).text(),
-        //limit : 10,
+        limit : 10,
     });
     //alernately: baseURL+"?"+"api_key=fuu2wetcetc"+"&"+"q=searchterms"+"&".... etc etc
     var queryURL = `${baseURL}?${params}`;
@@ -34,15 +39,16 @@ $(document).on("click", ".gif-button", function(){
         $gifTarget.empty();
         console.log(response);
         for(var i = 0; i < response.data.length; i++){
+            var newGifContainer = $("<div class='gif-container'></div>");
+            var newGifRating = $(`<div>Rating: ${response.data[i].rating}</div>`);
             var newGif = $("<img class='gif' data-state='still'>");
-            newGif.attr("data-still", response.data[i].images["fixed_height_small_still"].url);
-            newGif.attr("data-play", response.data[i].images["fixed_height_small"].url);
+            newGif.attr("data-still", response.data[i].images["fixed_height_still"].url);
+            newGif.attr("data-play", response.data[i].images["fixed_height"].url);
             newGif.attr("src", newGif.attr("data-still"));
-            $gifTarget.append(newGif);
+            newGifContainer.append(newGif, newGifRating);
+            $gifTarget.append(newGifContainer);
         }
     });
-
-    //newGif.attr("src", response.data[i].images["fixed_height_small"].url);
 });
 
 $(document).on("click", ".gif", function(){
@@ -54,4 +60,17 @@ $(document).on("click", ".gif", function(){
         $this.attr("data-state", "still");
         $this.attr("src", $this.attr("data-still"));
     }
+});
+
+$addButton.on("click", function(){
+    event.preventDefault();
+
+    //get val().trim() from input form
+    var input = $addInput.val().trim();
+
+    //add to buttonsDiv
+    var newButton = $(`<button class="gif-button btn btn-primary">${input}</button>`);
+    buttonDiv.append(newButton);
+
+    $addInput.val("");
 });
